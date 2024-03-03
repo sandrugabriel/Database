dab29fcc3fbbf42335dd7d0a840d3862847191cb
sandrugabriel <sandru11.gabriel@gmail.com>
2024-03-03 17:43:09 +0200

    gitignore

---
+++ exerciti.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users(
+    user_id INT PRIMARY KEY,
+    name_user VARCHAR(20),
+    age_user INT,
+    email_user VARCHAR(50),
+    birthday_user DATETIME
+);
+
+create  database test_db;
+use test_db;
+
+
+
